5aac4ecb04ca850e042e99924090a7750f2b046c
Alena0212 <alena0212@gmail.com>
2017-10-06 09:59:47 +0700

    Updated with new function definitions

--- Date.cpp
+++ Date.cpp
@@ -14,13 +14,44 @@ Date::Date(){
 	sec = utc_time->tm_sec;
 };
 
-Date::Date(unsigned int Y, unsigned int M, unsigned int D, unsigned int h, unsigned int m, unsigned int s) {
+Date::Date(unsigned int Y, Month M, unsigned int D) {
 	year = Y;
-	mon = Month(M);
+	mon = M;
+	day = D;
+	hour = 0;
+	min = 0;
+	sec = 0;
+};
+
+Date::Date(unsigned int h, unsigned int m, unsigned int s) {
+	time_t cur_time;
+	struct tm *cur_date;
+	time(&cur_time);
+	cur_date = gmtime(&cur_time);
+	year = cur_date->tm_year + 1900;
+	mon = Month(cur_date->tm_mon);
+	day = cur_date->tm_mday;
+	hour = h;
+	min = m;
+	sec = s;
+};
+
+Date::Date(unsigned int Y, Month M, unsigned int D, unsigned int h, unsigned int m, unsigned int s) {
+	year = Y;
+	mon = M;
 	day = D;
 	hour = h;
 	min = m;
 	sec = s;
+};
+
+Date::Date(const Date &date) {
+	year = date.year;
+	mon = date.mon;
+	day = date.day;
+	hour = date.hour;
+	min = date.min;
+	sec = date.sec;
 };
 
 unsigned int Date::get_year() {
@@ -48,25 +79,28 @@ unsigned int Date::get_seconds() {
 };
 
 Date Date::add_years(unsigned int Y) const {
-	
+	Date result(*this);
+	result.year += Y;
+	return result;
 };
 
-Date Date::add_months(unsigned int M) const {
-
-};
-
-Date Date::add_days(unsigned int D) const {
-
-};
-
-Date Date::add_hours(unsigned int h)  const {
-
-};
-
-Date Date::add_minutes(unsigned int m) const {
-
-};
-
-Date Date::add_seconds(unsigned int s) const {
-
-};
+//
+//Date Date::add_months(Month M) const {
+//
+//};
+//
+//Date Date::add_days(unsigned int D) const {
+//
+//};
+//
+//Date Date::add_hours(unsigned int h)  const {
+//
+//};
+//
+//Date Date::add_minutes(unsigned int m) const {
+//
+//};
+//
+//Date Date::add_seconds(unsigned int s) const {
+//
+//};
--- Date.h
+++ Date.h
@@ -1,21 +1,24 @@
 #pragma once
 
+enum Month {
+	January = 0, February, March, April, May, June,
+	July, August, September, October, November, December
+};
+
 class Date {
 private:
 	unsigned int year;
-	enum Month {
-		January = 0, February, March, April, May, June, 
-		July, August, September, October, November, December 
-	} mon;
 	unsigned int day;
+	Month mon;
 	unsigned int hour;
 	unsigned int min;
 	unsigned int sec;
 public:
 	Date();
-	//Date(unsigned int Y, Month M, unsigned int D); 
-	//Date(unsigned int h, unsigned int m, unsigned int s); not certain how to distinguish these two constructors
-	Date(unsigned int Y, unsigned int M, unsigned int D, unsigned int h, unsigned int m, unsigned int s);
+	Date(unsigned int Y, Month M, unsigned int D); 
+	Date(unsigned int h, unsigned int m, unsigned int s); 
+	Date(unsigned int Y, Month M, unsigned int D, unsigned int h, unsigned int m, unsigned int s);
+	Date(const Date &date);
 	unsigned int get_year();
 	unsigned int get_month();
 	unsigned int get_day();
@@ -23,7 +26,7 @@ public:
 	unsigned int get_minutes();
 	unsigned int get_seconds();
 	Date add_years(unsigned int Y) const;
-	Date add_months(unsigned int M) const;
+	Date add_months(Month M) const;
 	Date add_days(unsigned int D) const;
 	Date add_hours(unsigned int h) const;
 	Date add_minutes(unsigned int m) const;
